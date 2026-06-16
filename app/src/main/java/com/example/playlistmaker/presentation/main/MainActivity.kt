b76a460d0f674e3da1b3fee59c138efc4e3ae889
Dilyara <dilya_19@inbox.ru>
2026-06-16 13:58:09 +0400

    Hide bottom navigation on audio player

--- app/src/main/java/com/example/playlistmaker/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/presentation/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.playlistmaker.presentation.main
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.playlistmaker.R
@@ -22,5 +23,9 @@ class MainActivity : AppCompatActivity() {
 
         val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigation)
         bottomNavigation.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            bottomNavigation.isVisible = destination.id != R.id.audioPlayerFragment
+        }
     }
 }
