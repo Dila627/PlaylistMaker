16e54323e9b8571a2cad28c6f146c8adff0d2239
Dilyara <dilya_19@inbox.ru>
2026-06-16 15:20:34 +0400

    Fix media library fragment tabs

--- app/src/main/java/com/example/playlistmaker/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/presentation/main/MainActivity.kt
@@ -12,7 +12,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
--- app/src/main/java/com/example/playlistmaker/presentation/medialibrary/MediaLibraryFragment.kt
+++ app/src/main/java/com/example/playlistmaker/presentation/medialibrary/MediaLibraryFragment.kt
@@ -3,7 +3,10 @@ package com.example.playlistmaker.presentation.medialibrary
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.example.playlistmaker.R
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MediaLibraryFragment : Fragment(R.layout.fragment_media_library) {
 
@@ -13,5 +16,17 @@ class MediaLibraryFragment : Fragment(R.layout.fragment_media_library) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val viewPager = view.findViewById<ViewPager2>(R.id.viewPager)
+        val tabLayout = view.findViewById<TabLayout>(R.id.tabLayout)
+
+        viewPager.adapter = MediaLibraryPagerAdapter(requireActivity())
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = when (position) {
+                0 -> getString(R.string.favorite_tracks)
+                else -> getString(R.string.playlists)
+            }
+        }.attach()
     }
 }
