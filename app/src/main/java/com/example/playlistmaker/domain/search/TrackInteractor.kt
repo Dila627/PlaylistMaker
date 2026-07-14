11fdc3d48f7cfd54ac1b25dc33b36ad8e01d854a
Dilara Bakhishova <dilya_19@inbox.ru>
2026-07-14 15:35:52 +0400

    Migrate track search to coroutines and Flow

--- app/src/main/java/com/example/playlistmaker/domain/search/TrackInteractor.kt
+++ app/src/main/java/com/example/playlistmaker/domain/search/TrackInteractor.kt
@@ -1,7 +1,9 @@
 package com.example.playlistmaker.domain.search
 
 import com.example.playlistmaker.domain.models.TrackSearchResult
+import kotlinx.coroutines.flow.Flow
 
 interface TracksInteractor {
-    fun searchTracks(expression: String, consumer: (TrackSearchResult) -> Unit)
+
+    fun searchTracks(expression: String): Flow<TrackSearchResult>
 }
